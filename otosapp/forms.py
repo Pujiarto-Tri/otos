9623088281b0b5ab182f9902b55843d0f30244ce
pujiarto_ta <pujiarto.ta@gmail.com>
2025-06-16 12:23:11 +0800

    modified:   otosapp/forms.py

--- otosapp/forms.py
+++ otosapp/forms.py
@@ -129,6 +129,12 @@ class CategoryUpdateForm(forms.ModelForm):
 
 class QuestionForm(forms.ModelForm):
     question_text = forms.CharField(widget=CKEditor5Widget(config_name='extends'))
+    category = forms.ModelChoiceField(
+        queryset=Category.objects.all(),
+        widget=forms.Select(attrs={
+            'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500'
+        })
+    )
     
     class Meta:
         model = Question
