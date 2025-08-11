9f994c26fc77af269d12e014ad522dcead9a391a
ajied <pujiarto.ta@gmail.com>
2025-08-11 19:09:28 +0800

    feat: Configure S3 media storage for production on Vercel

--- otos/settings.py
+++ otos/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'compressor',
     'django_ckeditor_5',
     'otosapp',
+    # Use django-storages for S3 media storage in production
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -141,7 +143,17 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # WhiteNoise configuration
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
-# Default primary key field type
+"""
+Use S3 for media storage in production on Vercel (read-only file system)
+"""
+if os.environ.get('VERCEL') or os.environ.get('VERCEL_URL'):
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+    AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME', None)
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
