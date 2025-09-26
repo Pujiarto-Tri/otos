cf49b49d582a162950fb64ca44c00c9861289e93
pujiarto <pujiarto.ta@gmail.com>
2025-09-26 08:26:25 +0800

    Add heroicons template tag for SVG icons in admin dashboard
    - Implemented a new template tag `heroicon` to return SVG strings for a minimal set of Heroicons (outline).
    - Added SVG definitions for icons: currency, calendar, users, clipboard, play, and document.
    - The icons use `currentColor` for flexible styling based on parent container color.

---
+++ otosapp/templatetags/heroicons.py
@@ -0,0 +1,24 @@
+from django import template
+from django.utils.safestring import mark_safe
+
+register = template.Library()
+
+# Minimal set of Heroicons (outline) used by admin dashboard. Return raw SVG with currentColor so
+# the parent container can control color via text-color utilities.
+HEROICONS = {
+    'currency': '''<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18.75a60.07 60.07 0 0 1 15.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 0 1 3 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 0 0-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 0 1-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 0 0 3 15h-.75M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm3 0h.008v.008H18V10.5Zm-12 0h.008v.008H6V10.5Z" /></svg>''',
+    'calendar': '''<svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1.5"><path stroke-linecap="round" stroke-linejoin="round" d="M8 7V3m8 4V3M3 11h18M5 21h14a2 2 0 0 0 2-2V7H3v12a2 2 0 0 0 2 2z"/></svg>''',
+    'users': '''<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" /></svg>''',
+    'clipboard': '''<svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1.5"><path stroke-linecap="round" stroke-linejoin="round" d="M9 12h6M9 16h6M3 7h18M5 7v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V7"/></svg>''',
+    'play': '''<svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1.5"><path stroke-linecap="round" stroke-linejoin="round" d="M5.25 5.653v12.694L18.75 12 5.25 5.653z"/></svg>''',
+    'document': '''<svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1.5"><path stroke-linecap="round" stroke-linejoin="round" d="M7.5 3.75h6l3 3v11.25A2.25 2.25 0 0 1 14.25 20.25H6.75A2.25 2.25 0 0 1 4.5 18V6a2.25 2.25 0 0 1 2.25-2.25zM9 9h6M9 12h6"/></svg>''',
+}
+
+
+@register.simple_tag
+def heroicon(name):
+    """Return an SVG string for the given heroicon name. Use in templates as:
+    {% heroicon 'calendar' as cal %}
+    Then pass cal into includes.
+    """
+    return mark_safe(HEROICONS.get(name, ''))
